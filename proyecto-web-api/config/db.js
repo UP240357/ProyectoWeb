a38b0c1bd4f20ba1874377ee52e34202792a4992
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-11 15:35:11 -0600

    Agregacion de /config/db.js

--- proyecto-web-api/config/db.js
+++ proyecto-web-api/config/db.js
@@ -2,5 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const datos = mysql.createPool({
-    host: process.env.db_uuu
+    host: process.env.db_Host,
+    user: process.env.db_usuario,
+    password: process.env.db_contrasenia,
+    database: process.env.db_nombre
 });
+
+module.exports = datos;
