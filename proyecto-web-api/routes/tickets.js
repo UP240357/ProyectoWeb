6918c698bcf88f1ae07b48ee8be3239da8f217b9
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-11 22:40:36 -0600

    .

--- proyecto-web-api/routes/tickets.js
+++ proyecto-web-api/routes/tickets.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ticketCtrl = require('../controllers/ticketController');
-const kpiCtrl = require('../controllers/kpiController');
 router.post('/tickets', ticketCtrl.createTicket);
 router.get('/tickets', ticketCtrl.getTickets);
 router.patch('/tickets/:id/status', ticketCtrl.updateStatus);
 router.post('/tickets/assign', ticketCtrl.assignTicket);
-router.get('/kpi/tickets/status', kpiCtrl.getTicketsByStatus);
-router.get('/kpi/tickets/user', kpiCtrl.getTicketsByUser);
+
 
 module.exports = router;
