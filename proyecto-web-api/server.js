78922631611fe4648352648b86ac841680e7b086
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-11 20:38:53 -0600

    .

--- proyecto-web-api/server.js
+++ proyecto-web-api/server.js
@@ -1,13 +1,23 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const ticketRoutes = require('./routes/tickets');
 const app = express();
-const PORT = 3000;
-
+app.use(cors());
 app.use(express.json());
-
-app.get("/api/saludo", (req, res) => {
-  res.json({mensaje: "hola joto"})
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/tickets', ticketRoutes);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Error interno del servidor' });
 });
-
-app.listen(PORT, ()=> {
-  console.log(`Si jala el server, esta en http://localhost:${PORT}`)
-})
+app.use((req, res) => {
+    res.status(404).json({ error: 'Endpoint no encontrado' });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
