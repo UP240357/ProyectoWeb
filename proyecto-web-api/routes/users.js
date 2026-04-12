06335a2cae3c8b3b05adad45661da76efadee143
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-11 21:31:41 -0600

    .

--- proyecto-web-api/routes/users.js
+++ proyecto-web-api/routes/users.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/db');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 router.post('/auth/login', authController.login);
 router.post('/users', userController.createUser);
 router.get('/users', async (req, res) => {
-    const [rows] = await db.query('SELECT id, name, email, rol FROM Users');
-    res.json(rows);
+    try {
+        const [rows] = await db.query('SELECT id, name, email, rol FROM Users');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ message: "Error en la base de datos" });
+    }
 });
-router.put('/users/:id', userController.updateUser);
-
 module.exports = router;
