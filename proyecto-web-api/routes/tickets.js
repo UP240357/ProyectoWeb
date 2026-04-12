60ccc0f1722710d99ae8a1509d06133f00b4dedf
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-12 15:44:02 -0600

    .

--- proyecto-web-api/routes/tickets.js
+++ proyecto-web-api/routes/tickets.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ticketController = require('../controllers/ticketController');
-router.post('/tickets', ticketController.createTicket); // [cite: 156]
-router.get('/tickets', ticketController.getTickets); // [cite: 159]
-router.get('/tickets/filter', ticketController.getTickets); // Reutiliza la lógica de filtros dinámicos [cite: 176]
-router.get('/tickets/:id', ticketController.getTicketById); // [cite: 173]
-router.put('/tickets/:id', ticketController.updateTicket); // [cite: 178]
-router.patch('/tickets/:id/status', ticketController.updateStatus); // [cite: 180]
-router.delete('/tickets/:id', ticketController.deleteTicket); // [cite: 182]
-router.post('/tickets/assign', ticketController.assignTicket); // [cite: 183]
-router.get('/tickets/user/:id', ticketController.getTicketsByUser); // [cite: 186]
+router.post('/tickets', ticketController.createTicket); 
+router.get('/tickets', ticketController.getTickets); 
+router.get('/tickets/filter', ticketController.getTickets);
+router.get('/tickets/:id', ticketController.getTicketById);
+router.put('/tickets/:id', ticketController.updateTicket);
+router.patch('/tickets/:id/status', ticketController.updateStatus);
+router.delete('/tickets/:id', ticketController.deleteTicket);
+router.post('/tickets/assign', ticketController.assignTicket);
+router.get('/tickets/user/:id', ticketController.getTicketsByUser);
 
 module.exports = router;
