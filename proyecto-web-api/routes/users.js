65d05577e59dff358d2d62e16f085ee66703c118
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-11 20:32:29 -0600

    .

--- proyecto-web-api/routes/users.js
+++ proyecto-web-api/routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-
 router.post('/', userController.createUser);
 router.get('/', userController.getAllUsers);
 router.patch('/:id/status', userController.updateStatus);
