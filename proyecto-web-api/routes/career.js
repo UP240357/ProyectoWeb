9413654e99ec5ad76ac080e263ccd430dba4ac1c
UP250004 <up250004@alumnos.upa.edu.mx>
2026-04-11 22:07:29 -0600

    .

--- proyecto-web-api/routes/career.js
+++ proyecto-web-api/routes/career.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const careerController = require('../controllers/careerController');
 router.get('/careers', careerController.getCareers);
+router.post('/careers', careerController.createCareer);
 router.get('/careers/filter', careerController.filterCareers);
 module.exports = router; 
